d12095f389b9dfa844071d8cd6468f9afd0f2ce7
Davide Pistilli <davidepistilli173@gmail.com>
2020-01-09 22:51:58 +0100

    Added texture generation from text

--- TheMissingSun/classes/TMS_Texture.cpp
+++ TheMissingSun/classes/TMS_Texture.cpp
@@ -20,36 +20,20 @@ TMS_Texture::TMS_Texture(const std::string fileName)
     /* Load the file into an SDL surface. */
     tms::surface_t textureSurface(
         IMG_Load(fileName.c_str()),
-        [](SDL_Surface* surf) {SDL_FreeSurface(surf); }
+        [](SDL_Surface* surf) { SDL_FreeSurface(surf); }
     );
     if (textureSurface == nullptr) throw "Failed to load texture " + fileName + "\n " + IMG_GetError();
+    _loadTexture(textureSurface);
+}
 
-    /* Generate a new OpenGL texture. */
-    glGenTextures(1, &_id);
-    glBindTexture(GL_TEXTURE_2D, _id);
-
-    /* Set default texture settings. */
-    _wrapX = tms::texture::DEFAULT_X_WRAP;
-    _wrapY = tms::texture::DEFAULT_Y_WRAP;
-    _minFilter = tms::texture::DEFAULT_MIN_FILTER;
-    _magFilter = tms::texture::DEFAULT_MAG_FILTER;
-
-    /* Set the behaviour when outside texture coordinates. */
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, tms::texture::DEFAULT_X_WRAP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, tms::texture::DEFAULT_Y_WRAP);
-    /* Set the texel selection/interpolation crtiteria. */
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, tms::texture::DEFAULT_MIN_FILTER);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, tms::texture::DEFAULT_MAG_FILTER);
-
-    /* Retrieve the image format. */
-    int imageFormat = GL_RGB;
-    if (textureSurface->format->BytesPerPixel == 4) imageFormat = GL_RGBA;
-
-    /* Load the texture into memory. */
-    glTexImage2D(GL_TEXTURE_2D, 0, imageFormat, textureSurface->w, textureSurface->h, 0, imageFormat, GL_UNSIGNED_BYTE, textureSurface->pixels);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    glBindTexture(GL_TEXTURE_2D, 0);
+TMS_Texture::TMS_Texture(const std::string text, const SDL_Color textColour, const tms::font_t& font)
+{
+    tms::surface_t textureSurface(
+        TTF_RenderText_Solid(font.get(), text.c_str(), textColour),
+        [](SDL_Surface* surf) { SDL_FreeSurface(surf); }
+    );
+    if (textureSurface == nullptr) throw "Failed to generate texture from text " + text + "\n " + IMG_GetError();
+    _loadTexture(textureSurface);
 }
 
 TMS_Texture::TMS_Texture(TMS_Texture&& oldTexture) noexcept
@@ -80,4 +64,34 @@ TMS_Texture& TMS_Texture::operator=(TMS_Texture&& oldTexture) noexcept
         _magFilter = oldTexture._magFilter;
     }
     return *this;
+}
+
+void TMS_Texture::_loadTexture(tms::surface_t& textureSurface)
+{
+    /* Generate a new OpenGL texture. */
+    glGenTextures(1, &_id);
+    glBindTexture(GL_TEXTURE_2D, _id);
+
+    /* Set default texture settings. */
+    _wrapX = tms::texture::DEFAULT_X_WRAP;
+    _wrapY = tms::texture::DEFAULT_Y_WRAP;
+    _minFilter = tms::texture::DEFAULT_MIN_FILTER;
+    _magFilter = tms::texture::DEFAULT_MAG_FILTER;
+
+    /* Set the behaviour when outside texture coordinates. */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, tms::texture::DEFAULT_X_WRAP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, tms::texture::DEFAULT_Y_WRAP);
+    /* Set the texel selection/interpolation crtiteria. */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, tms::texture::DEFAULT_MIN_FILTER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, tms::texture::DEFAULT_MAG_FILTER);
+
+    /* Retrieve the image format. */
+    int imageFormat = GL_RGB;
+    if (textureSurface->format->BytesPerPixel == 4) imageFormat = GL_RGBA;
+
+    /* Load the texture into memory. */
+    glTexImage2D(GL_TEXTURE_2D, 0, imageFormat, textureSurface->w, textureSurface->h, 0, imageFormat, GL_UNSIGNED_BYTE, textureSurface->pixels);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
