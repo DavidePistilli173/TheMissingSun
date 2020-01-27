d4b32d3fd9307dc64878348f3f4f4e57835cbfb7
Davide Pistilli <davidepistilli173@gmail.com>
2020-01-27 14:57:26 +0100

    Added files for EntityGrid data structure and Action struct.

---
+++ TheMissingSun/classes/TMS_Action.hpp
@@ -0,0 +1,10 @@
+#ifndef TMS_ACTION_HPP
+#define TMS_ACTION_HPP
+
+/* Struct containing information related to eventHandling performed by entities. */
+struct TMS_Action
+{
+
+};
+
+#endif
---
+++ TheMissingSun/classes/TMS_EntityGrid.hpp
@@ -0,0 +1,10 @@
+#ifndef TMS_ENTITYGRID_HPP
+#define TMS_ENTITYGRID_HPP
+
+/* Data structure for managing the ordered entity placement in the game world. */
+class TMS_EntityGrid
+{
+
+};
+
+#endif
